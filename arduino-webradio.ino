b751570889c0cc287d2758718966a210b9da03d4
kkoehler <github@kkoehler.com>
2017-12-09 13:52:18 +0100

    switched dependencies to git submodules

--- arduino-webradio.ino
+++ arduino-webradio.ino
@@ -1,7 +1,7 @@
 #include "src/ethercard/EtherCard.h"
 #include "src/ethercard/net.h"
 #include "src/ethercard/enc28j60.h"
-#include <VS1053.h>
+#include "src/VS1053/VS1053.h"
 
 uint8_t Ethernet::buffer[1400]; // configure buffer size to 700 octets
 static uint8_t mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 }; // define (unique on LAN) hardware (MAC) address
@@ -18,82 +18,6 @@ VS1053  player(9, 6, 7, 8);
 const char website1[] PROGMEM = "stream.morow.com";
 const char website2[] PROGMEM = "swr-mp3-m-swr3.akacast.akamaistream.net";
 const char website3[] PROGMEM = "stream-eu1.radioparadise.com";
-
-//the length of the buffer between the meta info in stream
-//aka the value of 'icy-metaint'
-int metaLen = -1;
-
-boolean headerParsed = false;
-
-char * getHeaderValue(char *line, char *key) {
-
-  char* line_copy = malloc(strlen(line));
-  strcpy(line_copy, line);
-
-  int compare = strncmp(line, key, strlen(key));
-
-  if (compare != 0) {
-    return NULL;
-  }
-
-  char* s = strtok(line_copy, ":");
-  s = strtok(NULL, "\n");
-
-  free(line_copy);
-
-  return s;
-}
-
-int parseHTTPHeader(int *position, int maxLength) {
-
-  char *ptr;
-  char *startPtr;
-
-  //position in the buffer
-  int pos = position;
-
-  //length of a single line
-  int lineLength = -1;
-
-  while (lineLength != 0) {
-
-    startPtr = &Ethernet::buffer[pos];
-
-    for (ptr = startPtr ; *ptr != '\n'; ptr++) {
-      lineLength = ptr - startPtr;
-    }
-
-    char line[lineLength];
-    memcpy(line, startPtr, lineLength);
-
-    line[lineLength] = '\0';
-
-    Serial.println(*position);
-    Serial.println(maxLength);
-    Serial.print(line);
-    Serial.println("|");
-    Serial.println(lineLength);
-
-    if (lineLength != 0 && metaLen == -1) {
-
-      char *value = getHeaderValue (line, "icy-metaint");
-      if (value != NULL) {
-        metaLen = atoi(value);
-      }
-    }
-
-    pos += (lineLength + 2);
-
-    if (pos >= (position + maxLength)) {
-      Serial.println("length check problem");
-      break;
-    }
-
-  }
-
-  headerParsed = true;
-  return pos;
-}
 
 int count = 0;
 
@@ -158,7 +82,7 @@ void setup() {
   //ether.browseUrl(PSTR("/mp3-128"), "", website3, "", my_callback);
 
   player.begin();
-  player.modeSwitch();
+//  player.modeSwitch();
   player.setVolume(0);
 
 }
